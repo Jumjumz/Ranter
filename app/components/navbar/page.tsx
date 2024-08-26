6f5977c5c464a04e88c9e064ef9cbebacec4bf9c
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-08-26 20:18:54 +0800

    testing

--- app/components/navbar/page.tsx
+++ app/components/navbar/page.tsx
@@ -1,5 +1,15 @@
 function NavBar() {
-  return <div className=" sticky top-0 h-14 w-full bg-indigo-800"></div>;
+  return (
+    <div className="flex flex-row justify-end sticky top-0 h-14 w-full bg-indigo-800">
+      <div className="w-96 h-auto content-center">
+        <ul className=" flex flex-row gap-6 font-mono text-lg">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
